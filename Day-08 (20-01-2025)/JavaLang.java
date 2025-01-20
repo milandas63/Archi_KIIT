31e4c3352fb5e83fc33c9ca9afe37c279818543b
Milan Das <45796126+milandas63@users.noreply.github.com>
2025-01-20 16:23:17 +0530

    Add files via upload

---
+++ Day-08 (20-01-2025)/JavaLang.java
@@ -0,0 +1,14 @@
+package com.packages;
+
+import java.util.Date;
+
+public class JavaLang {
+	Date date;
+	StringBuffer buffer;
+	
+	public static void main(String[] args) {
+		double amount = Math.pow(25,3);
+		System.out.println(amount);
+	}
+
+}
